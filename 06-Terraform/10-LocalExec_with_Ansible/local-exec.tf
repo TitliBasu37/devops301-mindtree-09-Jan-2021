eb73ff1664481df2ada5d1d0c057458d5e614030
TitliBasu37 <48563702+TitliBasu37@users.noreply.github.com>
2021-02-10 21:11:24 +0530

    Update local-exec.tf

--- 06-Terraform/10-LocalExec_with_Ansible/local-exec.tf
+++ 06-Terraform/10-LocalExec_with_Ansible/local-exec.tf
@@ -46,8 +46,10 @@ resource "aws_instance" "dev-app" {
 
 
   connection {
+    type  = "ssh"
     user = "ubuntu"
     private_key = file(var.pvt_key)
+    host = "${element(aws_instance.dev-app.*.public_ip, count.index)}"
  
    }
    
